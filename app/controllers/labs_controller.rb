16aadab36d3bf0c286c786b2bbe7f0ab3a2edce1
CJC15 <cjc15@192.168.1.109>
2016-11-29 21:37:36 -0500

    Labs ready, creating new Labs

---
+++ app/controllers/labs_controller.rb
@@ -0,0 +1,63 @@
+class LabsController < ApplicationController
+  def index
+    @lab = Lab.all
+  end
+
+  def new
+    @lab = Lab.new
+  end
+
+  def edit
+    find_lab
+    if @lab.nil?
+      flash[:error] = "Experimento Fallido, estamos en problemas :("
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @lab = Lab.new(lab_params)
+    if @lab.save
+      redirect_to labs_path
+    end
+  end
+
+  def destroy
+    find_lab
+    if @lab.destroy
+      flash[:success] = "Lab destruido correctamente"
+    else
+      flash[:error] = "Experimento Fallido, estamos en problemas :("
+    end
+    redirect_to labs_path
+  end
+
+  def update
+    find_lab
+    if @lab.update(lab_params)
+      flash[:sucess] = "Lab created!"
+      redirect_to labs_path
+    else
+      flash[:error] = "Experimento Fallido, estamos en problemas :("
+      render 'edit'
+    end
+  end
+
+  def show
+    find_lab
+    if @lab.nil?
+      flash[:error] = "Experimento Fallido, estamos en problemas :("
+      redirect_to root_path
+    end
+  end
+end
+
+private
+
+def find_lab
+  @lab = Lab.find_by(id: params[:id])
+end
+
+def lab_params
+  params.require(:lab).permit(:lab_name)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # Change for Users/sign In
+  root to: 'labs#new'
+  resources :labs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
