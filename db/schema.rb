358afdbfe2764a0f3cd985c7a95c080f64fbecde
CJC15 <cjc15@192.168.1.109>
2016-11-30 19:50:58 -0500

    Remove user_id from product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201001559) do
+ActiveRecord::Schema.define(version: 20161201004954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20161201001559) do
     t.text     "product_name"
     t.integer  "product_price"
     t.string   "product_image"
-    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
