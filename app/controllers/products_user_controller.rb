3b1f8d6721af369e7ccf7baffaba9e66fc0980e1
CJC15 <cjc15@192.168.1.109>
2016-12-05 21:43:14 -0500

    Products user controller W/o test

--- app/controllers/products_user_controller.rb
+++ app/controllers/products_user_controller.rb
@@ -1,33 +1,65 @@
 class ProductsUserController < ApplicationController
   before_action :authenticate_user!
   def index
-    @product_user = ProductsUser.all
+    @products_user = ProductsUser.all
   end
 
   def new
-    @products_user = ProductsUser.new
+    @product_user = ProductsUser.new
     @products = Product.all
     @labs = Lab.all
   end
 
   def edit
+    find_user_product
+    if @product_user.nil?
+      flash[:error] = "Problems"
+      redirect_to root_path
+    end
   end
 
   def create
+    @product_user = ProductsUser.new(product_user_params)
+    if @product_user.save
+      flash[:success] = "Created :D"
+      redirect_to product_user_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    find_user_product
+    if @product_user.destroy
+      flash[:success] = "Destroyed correctly"
+    else
+      flash[:error] = "We have problems manito!"
+    end
+    redirect_to products_user_path
   end
 
   def update
+    find_user_product
+    if @product_user(product_user_params)
+      flash[:success] = "Updated"
+      redirect_to product_user_path
+    else
+      flash[:error] = "We have problems :("
+      render 'edit'
+    end
   end
 
   def show
+    find_user_product
+    if @product_user.nil?
+      flash[:error] = "Problemitas"
+      redirect_to root_path
+    end
   end
 
   private
 
-  def find_product
+  def find_user_product
     @product_user = ProductUser.find_by(id: params[:id])
   end
 
